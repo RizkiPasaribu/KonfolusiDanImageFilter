5fc18d39a98b684090e7a792a78fb573d683d738
Rizki Pasaribu <innet71050500@gmail.com>
2022-06-28 20:09:27 +0700

    Memperbaiki nama fungsi biar lebih mudah di baca

---
+++ Konfolusi Dan Image Filtering/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace Konfolusi_Dan_Image_Filtering
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Browse_button = new System.Windows.Forms.Button();
+            this.OriginalPicture = new System.Windows.Forms.PictureBox();
+            this.GrayscalePicture = new System.Windows.Forms.PictureBox();
+            this.button_Grayscle = new System.Windows.Forms.Button();
+            this.FilterPicture = new System.Windows.Forms.PictureBox();
+            this.button_Filter = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.OriginalPicture)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GrayscalePicture)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.FilterPicture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Browse_button
+            // 
+            resources.ApplyResources(this.Browse_button, "Browse_button");
+            this.Browse_button.Name = "Browse_button";
+            this.Browse_button.UseVisualStyleBackColor = true;
+            this.Browse_button.Click += new System.EventHandler(this.Browse_button_Click);
+            // 
+            // OriginalPicture
+            // 
+            this.OriginalPicture.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            resources.ApplyResources(this.OriginalPicture, "OriginalPicture");
+            this.OriginalPicture.Name = "OriginalPicture";
+            this.OriginalPicture.TabStop = false;
+            // 
+            // GrayscalePicture
+            // 
+            this.GrayscalePicture.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            resources.ApplyResources(this.GrayscalePicture, "GrayscalePicture");
+            this.GrayscalePicture.Name = "GrayscalePicture";
+            this.GrayscalePicture.TabStop = false;
+            // 
+            // button_Grayscle
+            // 
+            resources.ApplyResources(this.button_Grayscle, "button_Grayscle");
+            this.button_Grayscle.Name = "button_Grayscle";
+            this.button_Grayscle.UseVisualStyleBackColor = true;
+            this.button_Grayscle.Click += new System.EventHandler(this.Convert_Grayscle_button_Click);
+            // 
+            // FilterPicture
+            // 
+            this.FilterPicture.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            resources.ApplyResources(this.FilterPicture, "FilterPicture");
+            this.FilterPicture.Name = "FilterPicture";
+            this.FilterPicture.TabStop = false;
+            // 
+            // button_Filter
+            // 
+            resources.ApplyResources(this.button_Filter, "button_Filter");
+            this.button_Filter.Name = "button_Filter";
+            this.button_Filter.UseVisualStyleBackColor = true;
+            this.button_Filter.Click += new System.EventHandler(this.Filter_Image_button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            resources.ApplyResources(this, "$this");
+            this.Controls.Add(this.button_Filter);
+            this.Controls.Add(this.FilterPicture);
+            this.Controls.Add(this.button_Grayscle);
+            this.Controls.Add(this.GrayscalePicture);
+            this.Controls.Add(this.OriginalPicture);
+            this.Controls.Add(this.Browse_button);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.OriginalPicture)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GrayscalePicture)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.FilterPicture)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button Browse_button;
+        private PictureBox OriginalPicture;
+        private PictureBox GrayscalePicture;
+        private Button button_Grayscle;
+        private PictureBox FilterPicture;
+        private Button button_Filter;
+    }
+}
---
+++ Konfolusi Dan Image Filtering/Form1.cs
@@ -0,0 +1,101 @@
+namespace Konfolusi_Dan_Image_Filtering
+{
+    public partial class Form1 : Form
+    {
+        String fileLocation ="";
+        Bitmap? gambarGrayscale;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Browse_button_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            openFile.Title = "Browse Image";
+            openFile.Filter = "Choose Image (*.jpeg, *.png) |*jpeg;*png";
+
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                fileLocation = openFile.FileName;
+                Image image = Image.FromFile(fileLocation);
+                OriginalPicture.Image = image;
+            }
+        }
+
+        private void Convert_Grayscle_button_Click(object sender, EventArgs e)
+        {
+            //runtime eror handle
+            try
+            {
+                gambarGrayscale = new Bitmap(fileLocation);
+                Bitmap gambarOriginal = new Bitmap(fileLocation);
+                for (int y = 0; y < gambarOriginal.Height; y++)
+                {
+                    for (int x = 0; x < gambarOriginal.Width; x++)
+                    {
+                        Color c = gambarOriginal.GetPixel(x, y);
+
+                        int alpha = c.A;
+                        int red = c.R;
+                        int green = c.G;
+                        int blue = c.B;
+
+                        // convert to Grayscale
+                        int xg = (int)(red + green + blue) / 3;
+
+                        gambarGrayscale.SetPixel(x, y, Color.FromArgb(alpha, xg, xg, xg));
+                    }
+                }
+                GrayscalePicture.Image = gambarGrayscale;
+            }
+            catch (Exception eror)
+            {
+                MessageBox.Show(eror.Message, "Eror File Image Not Found");
+            }
+        }
+
+        private void Filter_Image_button_Click(object sender, EventArgs e)
+        {
+            //runtime eror handle
+            if (gambarGrayscale != null)
+            {
+                Bitmap FilterImage = new Bitmap(gambarGrayscale);
+
+                //karnel
+                float[] k =  { 0.5f ,        //   0  0.5  0
+                        0.5f, 0.5f, 0.5f,   // 0.5  0.5  0.5
+                              0.5f };       //   0  0.5  0
+                for (int y = 1; y < gambarGrayscale.Height - 1; y++)
+                {
+                    for (int x = 1; x < gambarGrayscale.Width - 1; x++)
+                    {
+                        Color c1 = gambarGrayscale.GetPixel(x - 1, y);
+                        Color c2 = gambarGrayscale.GetPixel(x + 1, y);
+                        Color c3 = gambarGrayscale.GetPixel(x, y - 1);
+                        Color c4 = gambarGrayscale.GetPixel(x, y + 1);
+                        Color c5 = gambarGrayscale.GetPixel(x, y);
+
+                        int w1 = c1.R;
+                        int w2 = c2.R;
+                        int w3 = c3.R;
+                        int w4 = c4.R;
+                        int w5 = c5.R;
+
+                        int h = (int)(k[0] * w1 + k[1] * w2 + k[2] * w3 + k[3] * w4 + +k[4] * w5);
+
+                        if (h < 0) h = 0;
+                        if (h > 255) h = 255;
+
+                        FilterImage.SetPixel(x, y, Color.FromArgb(h, h, h));
+                    }
+                }
+                FilterPicture.Image = FilterImage;
+            }
+            else
+            {
+                MessageBox.Show("Eror Gambar Belum Di Grayscale Atau Belum Dimuat", "Eror");
+            }
+        }
+    }
+}
